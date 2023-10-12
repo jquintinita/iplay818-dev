099e3faab6fa5b3002baa487311bd1091034d84b
jquintinita <joseph.quintinita@gmail.com>
2023-10-12 22:46:51 +0800

    updated

--- js/general.js
+++ js/general.js
@@ -9,4 +9,13 @@ $(function(){
         slidesToScroll: 1,
         
       });
+    
+    
+      $(".nav-menu-list > li").hover(function(){
+        console.log($(this).attr("data-nav"));
+        $("header").addClass("mega-menu-on")
+        $("header").on("mouseleave", function(){
+          $("header").removeClass("mega-menu-on")
+        })
+      })
 })
