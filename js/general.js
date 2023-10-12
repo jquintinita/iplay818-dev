7eef15e3bb4d332619fe7692b70b8998790b0fb8
jquintinita <joseph.quintinita@gmail.com>
2023-10-12 12:16:26 +0800

    push to main

---
+++ js/general.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.banner-slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        
+      });
+})
