4e58466c2ca99a9c31a32738d5c3227cbfac2378
jquintinita <joseph.quintinita@gmail.com>
2023-10-13 09:03:24 +0800

    update

--- js/general.js
+++ js/general.js
@@ -2,7 +2,7 @@ $(function(){
     $('.banner-slider').slick({
         dots: false,
         arrows: false,
-        infinite: false,
+        infinite: true,
         autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 1,
@@ -10,12 +10,58 @@ $(function(){
         
       });
     
+
+      $('.sports-megamenu').slick({
+        dots: false,
+        arrows: true,
+        infinite: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        
+      });
     
       $(".nav-menu-list > li").hover(function(){
         console.log($(this).attr("data-nav"));
-        $("header").addClass("mega-menu-on")
+        var navActive = $(this).attr("data-nav");
+        $("header").addClass("mega-menu-on");
+
+        $(".mega-submenu >div > div[data-submenu=" + navActive + "]").addClass("on").siblings().removeClass("on")
+
         $("header").on("mouseleave", function(){
           $("header").removeClass("mega-menu-on")
         })
       })
+
+      //DETECT MOBILE
+      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        window.location.replace("/mobile/");
+      }else{
+       
+      }
+
+      function isMobile() {
+        const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        return regex.test(navigator.userAgent);
+      }
+      if (isMobile()) {
+        window.location.replace("/mobile/");
+      } else {
+      
+      }
+
+
+      $(window).resize(function() {
+        // This will fire each time the window is resized:
+          if($(window).width() >= 1024) {
+              // if larger or equal
+              $('.element').show();
+          } else {
+              // if smaller
+              if (isMobile()) {
+                window.location.replace("/mobile/");
+              } else {
+              
+              }
+          }
+      }).resize(); // This will simulate a resize to trigger the initial run.
 })
---
+++ mobile/js/genera.js
@@ -0,0 +1,26 @@
+$(function(){
+
+    function isMobile() {
+        const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+        return regex.test(navigator.userAgent);
+      }
+      if (isMobile()) {
+            
+      } else {
+      window.location.replace("/");
+      }
+      $(window).resize(function() {
+        // This will fire each time the window is resized:
+
+        if (isMobile()) {
+                   
+        } else {
+          
+          if($(window).width() >= 1024) {
+                window.location.replace("/");
+                
+            }
+        }
+         
+      })
+})
