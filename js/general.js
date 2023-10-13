feb7d126d3c3ae1e4bd84af94ccd1a6cd8cc1fa1
jquintinita <joseph.quintinita@gmail.com>
2023-10-14 00:52:31 +0800

    added download section, top games and setup footer

--- js/general.js
+++ js/general.js
@@ -6,8 +6,7 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 3000,
         slidesToShow: 1,
-        slidesToScroll: 1,
-        
+        slidesToScroll: 1
       });
     
 
@@ -16,8 +15,7 @@ $(function(){
         arrows: true,
         infinite: false,
         slidesToShow: 3,
-        slidesToScroll: 1,
-        
+        slidesToScroll: 1
       });
 
       $('.casino-megamenu').slick({
@@ -25,16 +23,15 @@ $(function(){
         arrows: true,
         infinite: false,
         slidesToShow: 3,
-        slidesToScroll: 1,
-        
+        slidesToScroll: 1
       });
+
       $('.slot-megamenu').slick({
         dots: false,
         arrows: true,
         infinite: false,
         slidesToShow: 3,
-        slidesToScroll: 1,
-        
+        slidesToScroll: 1
       });
     
       
@@ -51,13 +48,7 @@ $(function(){
         })
       })
 
-      //DETECT MOBILE
-      if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        window.location.replace("mobile/");
-      }else{
-       
-      }
-
+     
       function isMobile() {
         const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
         return regex.test(navigator.userAgent);
@@ -83,4 +74,16 @@ $(function(){
               }
           }
       }).resize(); // This will simulate a resize to trigger the initial run.
+
+
+
+      //TAB Module 
+      $(".tab-control > li").click(function(){
+          $(this).addClass("active").siblings().removeClass("active");
+          var tabClick = $(this).attr("data-tab");
+
+          $(".tab-content-panel > div[data-tab-content= " + tabClick +"]").addClass("active").siblings().removeClass("active");
+      })
+
+
 })
