181e12b206230c7b07aa13c307eef07473b36eea
jquintinita <joseph.quintinita@gmail.com>
2023-10-15 17:35:56 +0800

    add mobile

--- mobile/js/genera.js
+++ mobile/js/genera.js
@@ -1,27 +1,23 @@
 $(function(){
 
-    function isMobile() {
-        const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
-        return regex.test(navigator.userAgent);
-      }
-      if (isMobile()) {
-            
-      } else {
-      window.location.replace("../");
-      }
-      $(window).resize(function() {
-        // This will fire each time the window is resized:
+  $('.banner-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
 
-        if (isMobile()) {
-                   
-        } else {
-          
-          if($(window).width() >= 1024) {
-                var pathname = window.location.pathname;
-                window.location.replace("../");
-                
-            }
-        }
-         
-      })
+  function isMobile() {
+    const regex = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+    return regex.test(navigator.userAgent);
+}
+if (isMobile()) {
+      
+} else {
+window.location.replace("../");
+}
+    
 })
